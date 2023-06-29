618b101e9318ee8fb635ea5ebd7704e7060ce321
一只鬆 <yzsong06@outlook.com>
2023-06-29 19:08:36 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 if [ ! -f "go-proxy-bingai" ];then
   echo Replit一键部署Go-Proxy-BingAI
   echo By The 一只鬆
-  echo 仓库开源地址：https://github.com/yzsong06/Replit_Go_BingAi/
+  echo 仓库开源地址：https://github.com/yzsong06/Replit-Go-Proxy-BingAi/
   curl -L https://github.com/adams549659584/go-proxy-bingai/releases/latest/download/go-proxy-bingai-linux-amd64.tar.gz -o bingai.tar.gz
   tar -zxvf bingai.tar.gz
   rm -f bingai.tar.gz
